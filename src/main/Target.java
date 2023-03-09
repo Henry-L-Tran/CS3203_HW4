4b7538ea9b4c94744a986b087e42e09c824cbc67
Henry-L-Tran <henrytran1129@gmail.com>
2023-03-08 21:28:37 -0600

    Updated code again.

--- src/main/Target.java
+++ src/main/Target.java
@@ -4,6 +4,10 @@ public class Target {
 
 	public boolean equal(int[] numbers, int target) {
 		
+		if(numbers.length <= 1) {
+			return false;
+		}
+		
 		for(int i = 0; i < numbers.length - 1; i++) {
 			for(int j = 1; j < numbers.length; j++) {
 				if(numbers[i] != numbers[j]) {
--- src/test/JTest.java
+++ src/test/JTest.java
@@ -9,11 +9,32 @@ import main.Target;
 public class JTest {
 
 	@Test
+	void arrayIsEmpty() {
+		var length = new Target();
+		int[] emptyArray = new int[5];
+		int[] filledArray = {10,11,12,13,14,15};
+		assertEquals(false, length.equal(emptyArray, 22));
+		assertEquals(true, length.equal(filledArray, 22));
+	}
+	
+	@Test
+	void arraySize1() {
+		var size = new Target();
+		int[] array1 = {3};
+		int[]array2 = {1,2,3};
+		assertEquals(false, size.equal(array1, 6));
+		//Same array size of 1, but the number is the target value.
+		assertEquals(false, size.equal(array1, 3));
+		assertEquals(true, size.equal(array2, 3));
+		
+	}
+	
+	@Test
 	void arrayDifferentNumbers() {
 		var check = new Target();
 		int[] arraySame = {2,2,3,4,4};
 		int[] arrayDifferent = {1,3,5,7,9};
-		assertEquals(false, check.equal(arraySame, 2));
+		assertEquals(false, check.equal(arraySame, 4));
 		assertEquals(true, check.equal(arrayDifferent, 12));
 	}
 	
@@ -35,17 +56,46 @@ public class JTest {
 		assertEquals(true, isTrue.equal(array2, 67));
 		
 	}
+		
 	@Test
-	void arrayOutOfBounds() {
-		var bounds = new Target();
-		int[] array = {1,2,3,4,5};
-		assertDoesNotThrow(IndexOutOfBoundsException.class, () -> bounds.equal(array, 4));
-	}
-	@Test
-	void arraySize5() {
+	void arrayElementsAdjacent() {
 		var targets = new Target();
 		int[] array = {1,2,3,4,5};
-		assertEquals(true, targets.equal(array, 4));
+		assertEquals(true, targets.equal(array, 3));
+		assertEquals(true, targets.equal(array, 5));
+		assertEquals(true, targets.equal(array, 7));
+		assertEquals(true, targets.equal(array, 9));
 		assertEquals(false, targets.equal(array, 10));
+	}
+	
+	@Test 
+	void arrayElementsFirstAndLast() {
+		var elements = new Target();
+		int[] array = {5,10,15,20,25,30,35};
+		int[] array2 = {3,6,9,12,15,18,21,24};
+		int[] array3 = {23,342,31,89,44,32,17};
+		assertEquals(true, elements.equal(array, 40));
+		assertEquals(true, elements.equal(array2, 27));
+		assertEquals(true, elements.equal(array3, 40));
+	}
+	
+	@Test
+	void arrayIsNegative() {
+		var negative = new Target();
+		int[] nNumbers = {-2,-3,-5,-11,-56,-32};
+		assertEquals(true, negative.equal(nNumbers, -34));
+		assertEquals(true, negative.equal(nNumbers, -5));
+		assertEquals(true, negative.equal(nNumbers, -16));
+		//Same integer but it's positive instead.
+		assertEquals(false, negative.equal(nNumbers, 16));
+	}
+	
+	@Test 
+	void arrayMultipleElementPairs() {
+		//Multiple element pairs that can add to target.
+		var multiple = new Target();
+		int[] array = {1,23,1,20,4,8,16,5,19,14,10};
+		assertEquals(true, multiple.equal(array, 24));
+		
 	}
 }
